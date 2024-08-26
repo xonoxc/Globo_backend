4bf5125f4c2fa3c51c52886981d5941299b4a93d
arpit <arpit2003y@gmail.com>
2024-08-26 12:09:23 +0530

    fix(cookies):cookies_option_added

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,6 +6,8 @@ export const BCRYPT_SALT_ROUNDS = 10
 export const COOKIE_OPTIONS: CookieOptions = {
      httpOnly: true,
      secure: true,
+     sameSite: "none",
+     path: "/",
 }
 
 export const FREE_POST_LIMIT = 10
