d786fff50cfe81f2f13f20f5078d6043837c9115
arpit <arpit2003y@gmail.com>
2024-09-04 22:08:22 +0530

    feat(schema):modified_contoller_file

--- src/controllers/like.controller.ts
+++ src/controllers/like.controller.ts
@@ -1,20 +1,53 @@
+import { prisma } from "../lib/prisma.client"
 import { ApiRequest } from "../types/ApiRequest"
-import { asyncHandler } from "../utils"
+import { Response } from "express"
+import { ApiError, ApiResponse, asyncHandler, uuidSchema } from "../utils"
 
-const createPostLike = asyncHandler(
+const togglePostLike = asyncHandler(async (req: ApiRequest, res: Response) => {
+     const { postId } = req.params
+
+     const validationResult = uuidSchema.safeParse(postId)
+     if (!validationResult.success) {
+          throw new ApiError("Invalid post id provided!", 400, [
+               { ...validationResult.error, name: "validtion error" },
+          ])
+     }
+     const parsedPostId = validationResult.data
+
+     const userId = req.user?.id as string
+
+     const result = await prisma.$transaction(async prisma => {
+          const existingLike = await prisma.like.findFirst({
+               where: {
+                    AND: [{ articleId: parsedPostId }, { userId: userId }],
+               },
+          })
+
+          if (!existingLike) {
+               return await prisma.like.create({
+                    data: {
+                         articleId: parsedPostId,
+                         userId: userId,
+                    },
+               })
+          }
+
+          return await prisma.like.delete({
+               where: {
+                    id: existingLike.id,
+               },
+          })
+     })
+
+     if (!result) throw new ApiError("Cannot toggle like", 500)
+
+     return res
+          .status(200)
+          .json(new ApiResponse(200, "like toggled successfully!", {}))
+})
+
+const toggleCommentLike = asyncHandler(
      async (req: ApiRequest, res: Response) => {}
 )
 
-const deletePostLike = asyncHandler(
-     async (req: ApiRequest, res: Response) => {}
-)
-
-const createCommentLike = asyncHandler(
-     async (req: ApiRequest, res: Response) => {}
-)
-
-const deleteCommentLike = asyncHandler(
-     async (req: ApiRequest, res: Response) => {}
-)
-
-export { createPostLike, deletePostLike, createCommentLike, deleteCommentLike }
+export { toggleCommentLike, togglePostLike }
